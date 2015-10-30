d88a7b64eb9535a92582b64b4bd2ba1ec9ee6d3e
cfang <chenfanggm@hotmail.com>
2015-10-29 19:57:41 -0700

    [cookbook] update cassandra cookbook

--- cookbooks/wolfer-cassandra/recipes/default.rb
+++ cookbooks/wolfer-cassandra/recipes/default.rb
@@ -9,17 +9,18 @@
 
 execute 'wget cassandra' do
   command 'wget -O /tmp/apache-cassandra.tar.gz http://www.apache.org/dyn/closer.lua/cassandra/3.0.0/apache-cassandra-3.0.0-rc2-bin.tar.gz'
-  notifies :run, 'untar cassandra', :immediately
+  notifies :run, 'execute[untar cassandra]', :immediately
 end
 
 execute 'untar cassandra' do
   command 'tar -xzvf /tmp/apache-cassandra.tar.gz -C /tmp/apache-cassandra'
   action :nothing
-  notifies :run, 'move cassandra to opt dir', :immediately
+  notifies :run, 'execute[move cassandra to opt dir]', :immediately  
 end
 
 execute 'move cassandra to opt dir' do
   command 'mv /tmp/apache-cassandra -C /opt/apache-cassandra'
   action :nothing
 end
+
 
