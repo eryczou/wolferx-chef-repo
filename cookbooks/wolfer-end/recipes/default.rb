0fc6b0f064dd1c05c1f40c85eecdb71564088688
cfang <chenfanggm@hotmail.com>
2015-11-01 18:07:48 -0800

    [cassandra] auto deploy testTable schema

--- cookbooks/wolfer-end/recipes/default.rb
+++ cookbooks/wolfer-end/recipes/default.rb
@@ -10,4 +10,10 @@
 
 service 'cassandra' do
   action [:enable, :restart]
+  notifies :run, 'execute[deploy test schema]', :immediately
+end
+
+execute 'deploy test schema' do
+  command 'cqlsh -f ~/chef-repo/cassandra_schema/testTable.cql'
+  action :nothing
 end
